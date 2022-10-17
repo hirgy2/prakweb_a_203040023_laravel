b4875e5708d0b7c952b99dcdf2282327cdf05db7
hirgy2 <m.rhyamizard.203040023@mail.unpas.ac.id>
2022-10-17 17:00:53 +0700

    menyelesaikan video 3 laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Muhammad Rhyamizard Hirgy",
+        "email" => "hirgy9@gmail.com",
+        "image" => "hirgy.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
