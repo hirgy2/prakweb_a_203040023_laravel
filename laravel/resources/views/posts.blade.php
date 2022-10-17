241011c8f255482c78ea7ad2c582fcb51c366594
hirgy2 <m.rhyamizard.203040023@mail.unpas.ac.id>
2022-10-17 23:10:40 +0700

    menyelesaikan video 4 Laravel tentang Blade Templating Engine

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hirgy Blog |  Posts</title>
-</head>
-<body>
-    <h1>Halaman Posts</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+
+@section('container')
+
+@foreach ($posts as $posts)
+<article class="mb-5">
+    <h2>
+        <a href="/posts/{{ $posts["slug"] }}">{{ $posts["title"] }}</h2></a>
+    <h5>By : {{ $posts["author"] }}</h5>
+    <p>{{ $posts["body"] }}</p>
+</article>
+@endforeach
+
+@endsection
+
+
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,17 +14,72 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
+        "title" => "About",
         "name" => "Muhammad Rhyamizard Hirgy",
         "email" => "hirgy9@gmail.com",
         "image" => "hirgy.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Rhyamizard HIrgy",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id distinctio enim rem natus repudiandae voluptas reprehenderit cum nesciunt laboriosam! Error deleniti amet ipsam commodi? Vel nam non exercitationem unde consequatur quam itaque fuga omnis odio, quod assumenda sapiente eveniet voluptatum natus id culpa. Tempora repudiandae obcaecati ullam tempore corrupti omnis maxime officia in quam ad ducimus quae quos dolor voluptate amet doloremque nesciunt voluptatem asperiores labore sunt, exercitationem animi laborum culpa consectetur? Porro eum perferendis dignissimos, recusandae mollitia cupiditate?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Iyan Karbu",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ipsum? Accusamus aspernatur beatae numquam, quo molestiae ipsa voluptatum, officia perferendis, officiis commodi quibusdam voluptate! Deleniti, nostrum odio maiores beatae officiis quasi distinctio quam culpa libero suscipit sequi commodi saepe corrupti aut! Corrupti omnis sit reprehenderit obcaecati ab dolores odio sapiente, suscipit rem recusandae molestiae officia ex dolorem modi voluptates est, doloremque sed? Ducimus nostrum libero ipsa vero, commodi sint quam. A quidem nobis, labore assumenda aliquid veritatis, voluptates corporis natus nam nulla praesentium cum voluptate beatae? Molestiae deleniti expedita eos quis aliquid sed sint est, ab accusantium nihil optio a."
+        ]
+    ];
+
+    return view('posts',[
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Rhyamizard HIrgy",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id distinctio enim rem natus repudiandae voluptas reprehenderit cum nesciunt laboriosam! Error deleniti amet ipsam commodi? Vel nam non exercitationem unde consequatur quam itaque fuga omnis odio, quod assumenda sapiente eveniet voluptatum natus id culpa. Tempora repudiandae obcaecati ullam tempore corrupti omnis maxime officia in quam ad ducimus quae quos dolor voluptate amet doloremque nesciunt voluptatem asperiores labore sunt, exercitationem animi laborum culpa consectetur? Porro eum perferendis dignissimos, recusandae mollitia cupiditate?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Iyan Karbu",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ipsum? Accusamus aspernatur beatae numquam, quo molestiae ipsa voluptatum, officia perferendis, officiis commodi quibusdam voluptate! Deleniti, nostrum odio maiores beatae officiis quasi distinctio quam culpa libero suscipit sequi commodi saepe corrupti aut! Corrupti omnis sit reprehenderit obcaecati ab dolores odio sapiente, suscipit rem recusandae molestiae officia ex dolorem modi voluptates est, doloremque sed? Ducimus nostrum libero ipsa vero, commodi sint quam. A quidem nobis, labore assumenda aliquid veritatis, voluptates corporis natus nam nulla praesentium cum voluptate beatae? Molestiae deleniti expedita eos quis aliquid sed sint est, ab accusantium nihil optio a."
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
