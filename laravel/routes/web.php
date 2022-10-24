cc3204ae021bf638f747e95ff43ee817be26751a
hirgy2 <m.rhyamizard.203040023@mail.unpas.ac.id>
2022-10-24 14:37:29 +0700

    menyelesaikan video 5 tentang model, collections & controllers

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,56 +32,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Muhammad Rhyamizard HIrgy",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id distinctio enim rem natus repudiandae voluptas reprehenderit cum nesciunt laboriosam! Error deleniti amet ipsam commodi? Vel nam non exercitationem unde consequatur quam itaque fuga omnis odio, quod assumenda sapiente eveniet voluptatum natus id culpa. Tempora repudiandae obcaecati ullam tempore corrupti omnis maxime officia in quam ad ducimus quae quos dolor voluptate amet doloremque nesciunt voluptatem asperiores labore sunt, exercitationem animi laborum culpa consectetur? Porro eum perferendis dignissimos, recusandae mollitia cupiditate?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Iyan Karbu",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ipsum? Accusamus aspernatur beatae numquam, quo molestiae ipsa voluptatum, officia perferendis, officiis commodi quibusdam voluptate! Deleniti, nostrum odio maiores beatae officiis quasi distinctio quam culpa libero suscipit sequi commodi saepe corrupti aut! Corrupti omnis sit reprehenderit obcaecati ab dolores odio sapiente, suscipit rem recusandae molestiae officia ex dolorem modi voluptates est, doloremque sed? Ducimus nostrum libero ipsa vero, commodi sint quam. A quidem nobis, labore assumenda aliquid veritatis, voluptates corporis natus nam nulla praesentium cum voluptate beatae? Molestiae deleniti expedita eos quis aliquid sed sint est, ab accusantium nihil optio a."
-        ]
-    ];
-
-    return view('posts',[
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Muhammad Rhyamizard HIrgy",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id distinctio enim rem natus repudiandae voluptas reprehenderit cum nesciunt laboriosam! Error deleniti amet ipsam commodi? Vel nam non exercitationem unde consequatur quam itaque fuga omnis odio, quod assumenda sapiente eveniet voluptatum natus id culpa. Tempora repudiandae obcaecati ullam tempore corrupti omnis maxime officia in quam ad ducimus quae quos dolor voluptate amet doloremque nesciunt voluptatem asperiores labore sunt, exercitationem animi laborum culpa consectetur? Porro eum perferendis dignissimos, recusandae mollitia cupiditate?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Iyan Karbu",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ipsum? Accusamus aspernatur beatae numquam, quo molestiae ipsa voluptatum, officia perferendis, officiis commodi quibusdam voluptate! Deleniti, nostrum odio maiores beatae officiis quasi distinctio quam culpa libero suscipit sequi commodi saepe corrupti aut! Corrupti omnis sit reprehenderit obcaecati ab dolores odio sapiente, suscipit rem recusandae molestiae officia ex dolorem modi voluptates est, doloremque sed? Ducimus nostrum libero ipsa vero, commodi sint quam. A quidem nobis, labore assumenda aliquid veritatis, voluptates corporis natus nam nulla praesentium cum voluptate beatae? Molestiae deleniti expedita eos quis aliquid sed sint est, ab accusantium nihil optio a."
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
