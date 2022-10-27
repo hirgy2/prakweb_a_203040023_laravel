2966a9cf6f13c7a7b291af4b9bd68a3d117decf2
hirgy2 <m.rhyamizard.203040023@mail.unpas.ac.id>
2022-10-27 22:53:42 +0700

    menyelesaikan video 11 tentang N+1 Problem

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -2,12 +2,15 @@
 
 
 @section('container')
+<h1 class="mb-5">{{ $title }}</h1>
 
 @foreach ($posts as $posts)
 <article class="mb-5 border-bottom pb-4">
     <h2><a href="/posts/{{ $posts->slug }}" class="text-decoration-none">{{ $posts->title }}</a></h2>
 
-    <p>By. <a href="/authors/{{ $posts->author->username }}" class="text-decoration-none">{{ $posts->author->name }}</a> In <a href="/categories/{{ $posts->category->slug }}"
+    <p>By. <a href="/authors/{{ $posts->author->username }}" 
+    class="text-decoration-none">{{ $posts->author->name }}</a>
+    In <a href="/categories/{{ $posts->category->slug }}"
     class="text-decoration-none">
     {{ $posts->category->name }}</a></p>
 
